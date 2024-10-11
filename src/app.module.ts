6b717fa21ffcedeba54dcdfdb2adc7ab56400aca
oumaymatr <trabelsioumaima@hotmail.com>
2024-10-12 00:39:31 +0200

    move MongoDB connection string to .env file for improved security

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { MoviesModule } from './movie/movie.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/movies_db'),
-    UserModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     MoviesModule,
     BookmarkModule,
     AuthModule,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,6 +23,7 @@ export class AuthService {
     const isPasswordValid = await bcrypt.compare(pass, user.mot_de_passe);
     if (isPasswordValid) {
       this.logger.debug(`Password valid for user: ${email}`);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { mot_de_passe, ...result } = user.toObject();
       return result;
     }
@@ -57,6 +58,7 @@ export class AuthService {
     });
 
     // Retourner l'utilisateur sans le mot de passe
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { mot_de_passe, ...result } = newUser.toObject();
     return result;
   }
